089544b7461397f3bd187f66e47790634d8f845e
Qays_ <635216690@qq.com>
2018-04-08 14:49:56 +0800

    warn fixed

---
+++ src/main/java/com/qays/bms/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.qays.bms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 04-08-2018 14:26
+ */
+@Controller
+@RequestMapping
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+
+        return "action/test";
+    }
+
+    @GetMapping("/test1")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
+}
