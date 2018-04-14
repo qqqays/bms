7569e0c72a2644ffd18245e5814ac70a5cf29c22
Qays_ <635216690@qq.com>
2018-04-14 22:07:18 +0800

    nothing todo

---
+++ src/test/java/com/qays/bms/GenericTest.java
@@ -0,0 +1,18 @@
+package com.qays.bms;
+
+import java.util.Set;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 04-14-2018 10:40
+ */
+
+public class GenericTest {
+
+    public <T extends Number> void fuck(T a ) {
+
+    }
+
+
+}
