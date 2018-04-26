418f89c73dc47ddb3daa71f977709f21e42f5754
Qays_ <635216690@qq.com>
2018-04-26 14:00:15 +0800

    add page_helper

---
+++ src/main/java/com/qays/bms/mapper/TestMapper.java
@@ -0,0 +1,28 @@
+package com.qays.bms.mapper;
+
+import com.github.pagehelper.PageInfo;
+import com.qays.bms.domain.JpaTestEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 04-26-2018 10:53
+ */
+@Mapper
+public interface TestMapper {
+
+    @Select("select * from test_table limit 1")
+    JpaTestEntity one();
+
+    @Select("select * from test_table")
+    List<JpaTestEntity> all();
+
+    @Select("select * from test_table")
+    List<JpaTestEntity> page();
+}
---
+++ src/test/java/com/qays/bms/MybatisTest.java
@@ -0,0 +1,58 @@
+package com.qays.bms;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.qays.bms.domain.JpaTestEntity;
+import com.qays.bms.mapper.TestMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Q-ays.
+ * whosqays@gmail.com
+ * 04-26-2018 10:56
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MybatisTest {
+
+    @Autowired
+    TestMapper tm;
+
+    @Test
+    @Rollback
+    public void fuck() {
+        System.out.println("------------------------------");
+        System.out.println(tm.one().getId() + tm.one().getContents());
+        System.out.println("------------------------------");
+    }
+
+    @Test
+    public void aa() {
+        for (JpaTestEntity jte : tm.all()) {
+            System.out.println();
+            System.out.println(jte.getId() + jte.getContents());
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void page() {
+        PageHelper.startPage(1, 3);
+
+        PageInfo<JpaTestEntity> page = new PageInfo<>(tm.page());
+
+        System.out.println(page);
+    }
+
+}
