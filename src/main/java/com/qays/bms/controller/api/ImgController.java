e1626324f4e3c66e0b5855c1009da0b94f7242db
qays <635216690@qq.com>
2018-05-08 11:14:46 +0800

    gains the absolute path of resources

--- src/main/java/com/qays/bms/controller/api/ImgController.java
+++ src/main/java/com/qays/bms/controller/api/ImgController.java
@@ -5,12 +5,16 @@ import com.qays.bms.common.exception.CustomException;
 import com.qays.bms.dao.ImgRepository;
 import com.qays.bms.domain.ImgEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
@@ -26,8 +30,10 @@ public class ImgController {
     private ImgRepository imgRepository;
 
     @GetMapping("test")
-    public String aa() {
-        return "success";
+    public String aa(HttpServletRequest request) throws NullPointerException {
+//        return WebUtils.getRealPath(request.getServletContext(), "resources");
+//        return request.getServletContext().getRealPath("/static/image");
+        return ClassUtils.getDefaultClassLoader().getResource("static/image").getPath().substring(1);
     }
 
     private String addImg(String relationPath,
