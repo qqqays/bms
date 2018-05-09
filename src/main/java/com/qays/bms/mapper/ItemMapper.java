a1fc05a93857b29513dc20abd813c39d3876ca2b
qays <635216690@qq.com>
2018-05-09 17:50:09 +0800

    bug fixed

--- src/main/java/com/qays/bms/mapper/ItemMapper.java
+++ src/main/java/com/qays/bms/mapper/ItemMapper.java
@@ -2,6 +2,7 @@ package com.qays.bms.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -15,5 +16,5 @@ public interface ItemMapper {
     @Insert("insert into dic_item(id, type_id, name, `value`, `sort`, `description`) " +
             "SELECT((SELECT id from dic_item where type_id = #{typeId} order by id desc limit 1) + 1), " +
             "#{typeId}, #{name}, #{value}, 5,#{description}")
-    Integer insertItem(String typeId, String name, String value, String description);
+    Integer insertItem(@Param("typeId") String typeId,@Param("name") String name,@Param("value") String value,@Param("description") String description);
 }
